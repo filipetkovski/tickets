588f386f694576f3a1beddec8c19e1b512f913a0
Filip Petkovski <filip.petkovski.work@gmail.com>
2025-01-12 12:28:03 +0100

    Fix error from the previous commit about the main class

--- src/main/java/project/ticket_manager/TicketManagerApplication.java
+++ src/main/java/project/ticket_manager/TicketManagerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TicketManagerApplication {
 
-	public static void mai(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(TicketManagerApplication.class, args);
 	}
 
